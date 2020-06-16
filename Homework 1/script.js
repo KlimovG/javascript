15f903b6b9002cdca6275674ae0910fd4d426601
Klimov Georgii <30409397+KlimovG@users.noreply.github.com>
2020-06-16 09:58:38 +0200

    update script.js

--- Homework 1/script.js
+++ Homework 1/script.js
@@ -1,25 +1,17 @@
-"use strict";
-
-let money = prompt("Ваш бюджет на месяц?", "50000");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "1991-04-02");
-    
-var appData = {
-    moneyData       : money,
-    timeData        : time,
-    expences        : {},
-    optionalExpences: {},
-    incomeData      : [],
-    savings         : false
+const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let exp1 = prompt("Введите обязательную статью расходов в этом месяце?", 
-"Бензин");
-let exp2 = prompt("Во сколько обойдется?", "10000");
-let exp3 = prompt("Введите обязательную статью расходов в этом месяце?", 
-" Еда");
-let exp4 = prompt("Во сколько обойдется?", "20000");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-appData.expences.exp1 = exp2;
-appData.expences.exp3 = exp4;
-
-alert(appData.moneyData/30);
+console.log(personalMovieDB);
